e51cfff80febfff3e1ddb5ae0bd08721cb37b97b
Projectoutlast <82677208+Projectoutlast@users.noreply.github.com>
2021-04-18 16:12:56 +0300

    Add files via upload

---
+++ knowledge.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 20;
+    $b = 10;
+    $c = $a + $b;
+    $d = $a - $b;
+    $e = $a / $b;
+    $f = $b ** 2;
+    ?>
+
+<?php include 'index.php'
---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+    $name = 'Виктор';
+    $group = 'HACKER-5';
+    $age = '29';
+    $city = 'Санкт-Петерубург';
+?>
+
+<?php include 'index.php' ?>
+
+
